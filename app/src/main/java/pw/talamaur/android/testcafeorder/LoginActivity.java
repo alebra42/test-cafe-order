c92fb19615e3baa0522b3e52e020f3f3a630814f
alebra42 <4082231+alebra42@users.noreply.github.com>
2019-01-25 22:55:32 +0700

    Empty button click event

--- app/src/main/java/pw/talamaur/android/testcafeorder/LoginActivity.java
+++ app/src/main/java/pw/talamaur/android/testcafeorder/LoginActivity.java
@@ -2,6 +2,7 @@ package pw.talamaur.android.testcafeorder;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onClickCreateOrder(View view) {
     }
 }
