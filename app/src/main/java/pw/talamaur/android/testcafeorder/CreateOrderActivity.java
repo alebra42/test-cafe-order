9a58f9576269b01878bdcedf77e6ff1ccd7fa1a6
alebra42 <4082231+alebra42@users.noreply.github.com>
2019-01-25 22:58:46 +0700

    Added new activity

---
+++ app/src/main/java/pw/talamaur/android/testcafeorder/CreateOrderActivity.java
@@ -0,0 +1,13 @@
+package pw.talamaur.android.testcafeorder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CreateOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_order);
+    }
+}
